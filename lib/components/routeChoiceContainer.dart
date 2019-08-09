ad8c5f7ac0f642b66f6c35767b80ca46f4f882ca
Mike Roberts <mikebroberts@hotmail.com>
2019-08-08 21:50:46 -0600

    commiting things that did not get auto added

---
+++ lib/components/routeChoiceContainer.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class RouteChoiceContainer extends StatelessWidget {
+  RouteChoiceContainer({this.routeName, this.bgImageUrl, this.description});
+
+  final String routeName;
+  final String bgImageUrl;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xff7c94b6),
+        image: DecorationImage(
+          colorFilter: ColorFilter.mode(Colors.black54, BlendMode.darken),
+          image: ExactAssetImage(bgImageUrl),
+          fit: BoxFit.cover,
+        ),
+        border: Border.all(
+          color: Colors.deepPurple,
+          width: 5.0,
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            routeName,
+            style: TextStyle(fontSize: 35, fontWeight: FontWeight.w900),
+          ),
+          Text(description,
+              style: TextStyle(fontSize: 25, fontStyle: FontStyle.italic))
+        ],
+      ),
+    );
+  }
+}
