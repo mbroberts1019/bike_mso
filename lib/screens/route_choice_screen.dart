36f171b96a27b81b1d32bd9a83b4170e290a3e37
Isaac German <isaacgerman95@gmail.com>
2019-08-29 20:17:35 -0600

    generated routes on route screen through firebase

--- lib/screens/route_choice_screen.dart
+++ lib/screens/route_choice_screen.dart
@@ -2,8 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:bike_mso/components/routeChoiceContainer.dart';
 import 'package:bike_mso/constants/polylines.dart';
 import 'package:bike_mso/screens/route_screen.dart';
+import 'package:firebase_database/firebase_database.dart';
+//import 'package:http/http.dart';
+//import 'package:loadmore/loadmore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class RideChoiceScreen extends StatelessWidget {
+class RideChoiceScreen extends StatefulWidget {
+  @override
+  _RideChoiceScreenState createState() => _RideChoiceScreenState();
+}
+
+class Routes {
+  String routeName;
+  String image;
+  String polyline;
+  String description;
+
+  Routes.fromJson(var value) {
+    this.routeName = value['routeName'];
+    this.image = value['image'];
+    this.description = value['description'];
+    this.polyline = value['polyline'];
+  }
+}
+
+class _RideChoiceScreenState extends State<RideChoiceScreen> {
+  final databaseReference = FirebaseDatabase.instance.reference();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,74 +36,78 @@ class RideChoiceScreen extends StatelessWidget {
         title: Text('Go Ride Bikes!!'),
       ),
       body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Expanded(
-              child: RouteChoiceContainer(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) {
-                      return RouteScreen(
-                        kKimWilliams,
-                        'Kickin\' it on the Kim',
+        child: StreamBuilder<QuerySnapshot>(
+            stream: Firestore.instance.collection('routes').snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              print(snapshot);
+
+              return ListView(
+                children:
+                    snapshot.data.documents.map((DocumentSnapshot document) {
+                  return RouteChoiceContainer(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) {
+                          return RouteScreen(
+                            document['polyline'],
+                            document['routeName'],
+                          );
+                        }),
                       );
-                    }),
+                    },
+                    bgImageUrl: 'assets/images/' + document['image'],
+                    description: document['description'],
+                    routeName: document['routeName'],
                   );
-                },
-                bgImageUrl: 'assets/images/kimWilliamsBridgeClub.jpg',
-                description:
-                    'A short dirt road ride along the Clark Fork east of town.',
-                routeName: 'Kickin\' it on the Kim',
-              ),
-            ),
-            Expanded(
-              child: RouteChoiceContainer(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) {
-                      return RouteScreen(
-                        kLoloRide,
-                        'Missoula to Lolo',
-                      );
-                    }),
-                  );
-                },
-                bgImageUrl: 'assets/images/loloTrail.JPG',
-                description: 'A pleasant ride up the Bitterrot Trail.',
-                routeName: 'Missoula to Lolo ',
-              ),
-            ),
-            Expanded(
-              child: RouteChoiceContainer(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) {
-                      return RouteScreen(
-                        kDeerCreekSneakPoly,
-                        'Deer Creek Sneak',
-                      );
-                    }),
-                  );
-                },
-                bgImageUrl: 'assets/images/deerCreekT29.jpg',
-                description:
-                    'A half dirt road half single track loop around the back side of Mt. Sentinal.',
-                routeName: 'Deer Creek Sneak',
-              ),
-            ),
-            Expanded(
-              child: Center(
-                child: Text('Route 5'),
-              ),
-            ),
-          ],
-        ),
+                }).toList(),
+              );
+            }),
       ),
     );
   }
 }
+
+//class RideChoiceScreen extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text('Go Ride Bikes!!'),
+//      ),
+//      body: Container(
+//        child: Column(
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          crossAxisAlignment: CrossAxisAlignment.stretch,
+//          children: <Widget>[
+//            Expanded(
+//              child: RouteChoiceContainer(
+//                onPressed: () {
+//                  Navigator.push(
+//                    context,
+//                    MaterialPageRoute(builder: (context) {
+//                      return RouteScreen(
+//                        kKimWilliams,
+//                        'Kickin\' it on the Kim',
+//                      );
+//                    }),
+//                  );
+//                },
+//                bgImageUrl: 'assets/images/kimWilliamsBridgeClub.jpg',
+//                description:
+//                    'A short dirt road ride along the Clark Fork east of town.',
+//                routeName: 'Kickin\' it on the Kim',
+//              ),
+//            ),
+//            Expanded(
+//              child: Center(
+//                child: Text('Route 5'),
+//              ),
+//            ),
+//          ],
+//        ),
+//      ),
+//    );
+//  }
+//}
